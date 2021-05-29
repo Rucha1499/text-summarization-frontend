1b6626add90b402c26b600aeea118b1fea5bdfae
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-05-29 12:24:30 +0530

    API call and getting data om frontend

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-const inputValue = document.getElementById("input-text");
+const BASE_URL = "http://127.0.0.1:8000/"
+
+const inputFile = document.getElementById("input-file")
+const submitButton = document.getElementById("submit-button")
+
+
+const createFormData = (files) => {
+    const formData = new FormData()
+    formData.append('file', files[0])
+    return formData
+}
+
+
+const handleSubmitEvent = async (formData) => {
+
+    console.log("heyy")
+    // fetch(BASE_URL,{
+    //     method: 'POST',
+    //     body: formData
+    // })
+    
+    const response = await fetch(BASE_URL, {
+        method: 'POST',
+        body: formData
+    })
+
+    
+    const data = await response.json()
+    console.log("data from API", data)
+    return data.output
+}
+
+submitButton.addEventListener('click', async (event) => {
+    document.getElementById("output").innerText = "Loading..."
+    const formData = createFormData(inputFile.files)
+    const displayThisData = await handleSubmitEvent(formData)
+    
+    document.getElementById("output").innerText = `${displayThisData}`
+})
