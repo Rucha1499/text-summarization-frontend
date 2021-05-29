0b1749e282689f942d7820fae5fe87cd5f650323
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-05-29 12:26:52 +0530

    removing commented code

--- main.js
+++ main.js
@@ -3,28 +3,19 @@ const BASE_URL = "http://127.0.0.1:8000/"
 const inputFile = document.getElementById("input-file")
 const submitButton = document.getElementById("submit-button")
 
-
 const createFormData = (files) => {
     const formData = new FormData()
     formData.append('file', files[0])
     return formData
 }
 
-
 const handleSubmitEvent = async (formData) => {
 
-    console.log("heyy")
-    // fetch(BASE_URL,{
-    //     method: 'POST',
-    //     body: formData
-    // })
-    
     const response = await fetch(BASE_URL, {
         method: 'POST',
         body: formData
     })
 
-    
     const data = await response.json()
     console.log("data from API", data)
     return data.output
